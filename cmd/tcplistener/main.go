a81a9e01c4db14fec1a3fe54ec544ab46b44afb4
Dargin Mathi <darginmathi@gmail.com>
2025-06-01 18:16:04 +0000

    Parse Header

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
+	"httpfromtcp/internal/request"
 	"log"
 	"net"
-	"strings"
 )
 
 const port = ":42069"
@@ -27,44 +25,15 @@ func main() {
 		fmt.Println("Accepted connection from", conn.RemoteAddr())
 
 		go func(c net.Conn) {
-			lines := getLinesChannel(c)
-			for line := range lines {
-				fmt.Println(line)
+			line, err := request.RequestFromReader(conn)
+			if err != nil {
+				log.Fatalf("%v", err)
 			}
+			fmt.Printf("Request line:\n")
+			fmt.Printf("- Method: %v\n", line.RequestLine.Method)
+			fmt.Printf("- Target: %v\n", line.RequestLine.RequestTarget)
+			fmt.Printf("- Version: %v\n", line.RequestLine.HttpVersion)
 			fmt.Println("Connection to ", conn.RemoteAddr(), "closed")
 		}(conn)
 	}
-}
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string)
-
-	go func() {
-		defer f.Close()
-		defer close(lines)
-		currentLine := ""
-		for {
-			buf := make([]byte, 8)
-			n, err := f.Read(buf)
-			if err != nil {
-				if currentLine != "" {
-					lines <- currentLine
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				break
-			}
-			str := string(buf[:n])
-			parts := strings.Split(str, "\n")
-
-			for i := 0; i < len(parts)-1; i++ {
-				lines <- fmt.Sprintf("%s%s", currentLine, parts[i])
-				currentLine = ""
-			}
-			currentLine += parts[len(parts)-1]
-		}
-	}()
-	return lines
 }
---
+++ internal/headers/headers.go
@@ -0,0 +1,68 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+const crlf = "\r\n"
+
+type Headers map[string]string
+
+func NewHeaders() Headers {
+	return map[string]string{}
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	idx := bytes.Index(data, []byte(crlf))
+	if idx == -1 {
+		return 0, false, nil
+	}
+	if idx == 0 {
+		return 2, true, nil
+	}
+
+	parts := bytes.SplitN(data[:idx], []byte(":"), 2)
+	key := strings.ToLower(string(parts[0]))
+
+	if key != strings.TrimRight(key, " ") {
+		return 0, false, fmt.Errorf("invalid header name: %s", key)
+	}
+
+	value := bytes.TrimSpace(parts[1])
+	key = strings.TrimSpace(key)
+	if !ValidToken([]byte(key)) {
+		return 0, false, fmt.Errorf("invalid header token found: %s", key)
+	}
+
+	h.Set(key, string(value))
+	return idx + 2, false, nil
+}
+
+func (h Headers) Set(key, value string) {
+	h[key] = value
+}
+
+var tokenChars = []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
+
+func ValidToken(data []byte) bool {
+	for _, c := range data {
+		if !(c >= 'A' && c <= 'Z' ||
+			c >= 'a' && c <= 'z' ||
+			c >= '0' && c <= 'z' ||
+			inTokenChars(c)) {
+			return false
+		}
+	}
+	return true
+}
+
+func inTokenChars(c byte) bool {
+	for _, t := range tokenChars {
+		if c == t {
+			return true
+		}
+	}
+	return false
+}
